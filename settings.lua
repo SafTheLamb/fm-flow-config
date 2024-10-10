0787ca90b8ea8d16362668e46c5557d23eebc38a
sapphiriza <sapphiriza@gmail.com>
2024-10-09 21:55:00 -0700

    1.0.9: Fluidic Power compat

--- settings.lua
+++ settings.lua
@@ -5,6 +5,6 @@ data:extend(
     type = "string-setting",
     name = "flow-config-denylist",
     setting_type = "startup",
-    default_value = "factory-,underwater-pipe-placer"
+    default_value = "factory-,underwater-pipe-placer,fluidic-"
   }
 })
